6d33213ca3edf6331399d79d08fbd895f7a9fcdc
gmail007 <37447167+gmail007@users.noreply.github.com>
2018-03-17 10:37:40 +0530

    login page html code

---
+++ logindesign.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+        <link rel="stylesheet" href="css/index.css" type="text/css">
+</head>
+<body>
+<section class="login-form">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 space">
+				<div class="log-in">
+				  <form action="" method="post">
+					<table align="center">
+						<tr>
+								<td><h1>Login In</h1></td>
+						</tr>
+						<tr>
+					        <td><input type="email" name="email" value=" " placeholder="Email address" required="required"></td>
+						</tr>
+						<tr>
+							<td><input type="password" name="password" value=" " placeholder="Password" required="required"></td>
+						</tr>
+						<tr>
+							<td><input class="login" type="submit" name="login" value="Login"></td>
+						</tr>
+						<tr>
+							<td><a class="signup" href="signup.php">Signup</a> </td>
+						</tr>
+					</table>
+					</form>
+				</div>
+			</div>x
+		</div>
+	</div>
+</section>
+</body>
+</html>
